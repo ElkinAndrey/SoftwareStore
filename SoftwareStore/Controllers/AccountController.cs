8c5658a02308819a8c524128053343a2967ef53c
ElkinAndrey <andreyridbir@gmail.com>
2022-09-08 21:30:20 +0500

    Creating Registration and Login Pages

---
+++ SoftwareStore/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(string returnURL)
+        {
+            return View();
+        }
+
+        public IActionResult Registration(string returnURL)
+        {
+            return View();
+        }
+    }
+}
