6a480aa8e956e5d8e2d10fc48a319ef7f2a4b225
ElkinAndrey <andreyridbir@gmail.com>
2022-09-08 21:08:29 +0500

    Adding Base Configurations to a Project

--- SoftwareStore/Program.cs
+++ SoftwareStore/Program.cs
@@ -1,6 +1,14 @@
+// Даниил Сергеевич. Эту часть кода я писал сам (ни кому за это не платил). Я оставляю здесь такие подробные коментарии для себя, чтобы в будущем понимать, что и зачем нужно, потому что это сложные непонятные слова
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews(); // Добавление MVC
+
 var app = builder.Build();
+app.UseHttpsRedirection(); // Принудительное применение HTTPS
+app.UseStaticFiles(); // Добавление статических фалов (css,  js)
+app.UseRouting(); // Добавляет соответствие маршрута в конвейер ПО промежуточного слоя
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); // От куда начинать
 app.Run();
