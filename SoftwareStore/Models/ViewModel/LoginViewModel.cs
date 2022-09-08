9d3023b2dece4a6d29800768396819f79200f147
ElkinAndrey <andreyridbir@gmail.com>
2022-09-08 21:37:34 +0500

    simple login view

---
+++ SoftwareStore/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftwareStore.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string ReturnURL { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
